ccc8b3b3bcd1f16782ac66635fc4a744103b17f8
Felipe Camargo <86023380+FGuerradecamargo@users.noreply.github.com>
2021-11-13 10:09:21 +0000

    first change

---
+++ percentage.py
@@ -0,0 +1,4 @@
+salary = float(input('Enter the current salary: £ '))
+#  a = 15/100 * s
+increase = salary * 15/100
+print('The new salary with 15% increased is: {:.2f}!'.format(salary + increase))
---
+++ type_is.py
@@ -0,0 +1,6 @@
+n = input('Type something: ')
+print('Is a number? ', n.isnumeric())
+print('Is letters? ', n.isalpha())
+print('Is decimal? ', n.isdecimal())
+print('Is lower? ', n.islower())
+
